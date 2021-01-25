7b57fcad13c7f458e3b96988246a2c5c74ec6ca0
pasfranc <pasfranc@gmail.com>
2021-01-25 15:13:02 +0100

    switch to gzip method

--- src/services/compress-service.ts
+++ src/services/compress-service.ts
@@ -4,13 +4,38 @@ const zlib = require('zlib');
 /* 
   This function is using zlib to compress a specific string
 */
-export async function compressString(stringToUpload:string) {
-  return zlib.deflateSync(stringToUpload).toString(BASE64_ENCODING);
+export function compressString(stringToUpload:string) {
+
+  let data = '';
+  zlib.gzip(stringToUpload, (err:any, buffer:any) => { 
+  
+    if (!err) { 
+      data +=buffer.toString(BASE64_ENCODING);
+       
+    }  
+    else { 
+      console.log("ERROR"+ err); 
+    }
+  });
+
+  return data;
 }
 
 /* 
   This function is using zlib to uncompress a specific string
 */
-export async function uncompressString(compressedString:string) {
-  return zlib.inflateSync(Buffer.from(compressedString, BASE64_ENCODING)).toString(); 
+export function uncompressString(compressedString:string) {
+  let data = '';
+  zlib.gunzip(compressedString, (err:any, buffer:any) => { 
+  
+    if (!err) { 
+      data +=buffer.toString(BASE64_ENCODING);
+       
+    }  
+    else { 
+      console.log("ERROR"+ err); 
+    }
+  });
+
+  return data;
 }
