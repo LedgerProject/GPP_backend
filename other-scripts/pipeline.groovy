f39568e9ca3dd362ec524a56f45f8c8ea245ad23
Pasquale Pio Francavilla <pasfranc@gmail.com>
2021-06-11 16:56:50 +0200

    Docker changes

--- other-scripts/pipeline.groovy
+++ other-scripts/pipeline.groovy
@@ -43,7 +43,7 @@ pipeline {
       steps {
         script {
 
-        def oldContainer = sh(script: 'docker ps -q --filter="name=gpp"', returnStdout: true)
+        def oldContainer = sh(script: 'docker ps -q -a --filter="name=gpp"', returnStdout: true)
 
         sh 'docker run -d --network="host" --name gpp-${BUILD_NUMBER} ${REPO_NAME}:latest'
         if (oldContainer){
